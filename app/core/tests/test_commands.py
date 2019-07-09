8430df8782f960cc5b9fe19b096816a3f582001a
NICK_LAPTOP\Nick <absolutelynick@gmail.com>
2019-07-09 07:46:46 +0100

    No module named 'psycopg2' issue

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,32 @@
+from unittest.mock import patch
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import TestCase
+
+
+class CommandTests(TestCase):
+
+    def test_wait_for_db_ready(self):
+        """ Test waiting for db when db is available. """
+
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as item:
+            item.return_value = True
+            call_command('wait_for_db')
+
+            # Checking that the db item was called and returned once
+            self.assertEqual(item.call_count, 1)
+
+    @patch('time.sleep', return_value=True)
+    def test_wait_for_db(self, ts):
+        """ Test waiting for db. Replaces the function of time.sleep. """
+
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as item:
+
+            # Side effect will raise the operation error 5 times and let it pass
+            # on the sixth
+            item.side_effect = [OperationalError] * 5 + [True]
+            call_command('wait_for_db')
+
+            # Asserting that we have reached the count of 6
+            self.assertEqual(item.call_count, 6)
