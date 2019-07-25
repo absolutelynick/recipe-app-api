f7d79b9ab749e2b41b8c0bee40eeb9fd1c21213b
nick.hughes <nick.hughes@touchsurgery.com>
2019-07-25 09:22:31 +0100

    Adding in the wait for database command that was erroring.

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -10,23 +10,23 @@ class CommandTests(TestCase):
     def test_wait_for_db_ready(self):
         """ Test waiting for db when db is available. """
 
-        with patch('django.db.utils.ConnectionHandler.__getitem__') as item:
-            item.return_value = True
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.return_value = True
             call_command('wait_for_db')
 
             # Checking that the db item was called and returned once
-            self.assertEqual(item.call_count, 1)
+            self.assertEqual(gi.call_count, 1)
 
     @patch('time.sleep', return_value=True)
     def test_wait_for_db(self, ts):
         """ Test waiting for db. Replaces the function of time.sleep. """
 
-        with patch('django.db.utils.ConnectionHandler.__getitem__') as item:
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
 
             # Side effect will raise the operation error 5 times and let it pass
             # on the sixth
-            item.side_effect = [OperationalError] * 5 + [True]
+            gi.side_effect = [OperationalError] * 5 + [True]
             call_command('wait_for_db')
 
             # Asserting that we have reached the count of 6
-            self.assertEqual(item.call_count, 6)
+            self.assertEqual(gi.call_count, 6)
