add3d459b4b8c97aeb730f1c810ab922e3ee0e75
nick.hughes <nick.hughes@touchsurgery.com>
2019-08-06 17:56:15 +0100

    Added ingredients api

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yk4+p*)_u+bb(!h^j51m!kg3&2aay$c9tq8hr0e%pa!u)#a*qq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -64,21 +64,21 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
 
-    # def test_create_ingredient_successful(self):
-    #     """Test creating a new ingredient"""
-    #     payload = {'name': 'Test tag'}
-    #     self.client.post(INGREDIENT_URL, payload)
-    #
-    #     exists = Ingredient.objects.filter(
-    #         user=self.user,
-    #         name=payload['name']
-    #     ).exists()
-    #
-    #     self.assertTrue(exists)
-    #
-    # def test_create_ingredient_invalid(self):
-    #     """Test creating a new ingredient with invalid payload. """
-    #     payload = {'name': ''}
-    #     res = self.client.post(INGREDIENT_URL, payload)
-    #
-    #     self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+    def test_create_ingredient_successful(self):
+        """Test creating a new ingredient"""
+        payload = {'name': 'Cabbage'}
+        self.client.post(INGREDIENT_URL, payload)
+
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+
+        self.assertTrue(exists)
+
+    def test_create_ingredient_invalid(self):
+        """Test creating a new ingredient with invalid payload. """
+        payload = {'name': ''}
+        res = self.client.post(INGREDIENT_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
