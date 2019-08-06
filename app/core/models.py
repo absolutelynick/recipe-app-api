1495be1fc1c2ca8aa7a0daab00ea07210ed04311
nick.hughes <nick.hughes@touchsurgery.com>
2019-08-06 16:15:31 +0100

    Adding all created files

--- app/core/models.py
+++ app/core/models.py
@@ -8,6 +8,8 @@ from django.contrib.auth.models import (
     PermissionsMixin
     )
 
+from django.conf import settings
+
 
 class UserManager(BaseUserManager):
 
@@ -46,3 +48,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Tag(models.Model):
+    """Tage to be used for a recipe"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
